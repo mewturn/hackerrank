686fdb9cec26b917aa46b7fff68f0a5311dfa2b0
mewturn <pathonice@hotmail.com>
2018-12-05 12:42:22 +0800

    Updated with growth pattern

--- Project Euler/207.py
+++ Project Euler/207.py
@@ -2,25 +2,28 @@ import math
 
 
 def partition(a, b):
-    perfect_count, total_count = 1, 1
-    m = 2
+    perfect_count, total_count = 0, 0
+    m = 0
+    growth = 2
 
-    while perfect_count/total_count >= a/b:
-        m += 1
+    while total_count == 0 or perfect_count/total_count >= a/b:
+        m += growth
+        growth += 2
         # Let x = 2 ** t (quadratic, two answers but ignore negative since 2 ** t strictly greater than 0)
         x_pos = 0.5 * (1 + ((1 + 4 * m) ** 0.5))
-        # x_neg = 0.5 * (1 - ((1 + 4 * k) ** 0.5))
+        # x_neg = 0.5 * (1 - ((1 + 4 * m) ** 0.5))
         t = math.log(x_pos, 2)
-        # print(m, x_pos, t)
 
-        # Check if x is an integer
+        # Check if x is an integer (should always be)
         if not x_pos % int(x_pos):
             total_count += 1
+            print(m, x_pos, t)
             # Check if t is an integer as well
             if not t % int(t):
                 perfect_count += 1
 
         # print(perfect_count / total_count)
+
     return m
 
 
